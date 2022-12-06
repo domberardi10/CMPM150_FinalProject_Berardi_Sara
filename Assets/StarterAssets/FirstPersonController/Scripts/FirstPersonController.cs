609008483e9237477e34bbba3c645f30d7157ec9
Menace791 <manas.s.sara@gmail.com>
2022-12-05 19:43:25 -0800

    Basic underwater movement implemented

--- Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -227,7 +227,14 @@ namespace StarterAssets
 			else
 			{
 				// reset the jump timeout timer
-				_jumpTimeoutDelta = JumpTimeout;
+				// _jumpTimeoutDelta = JumpTimeout;
+
+				// Jump
+				if (_input.jump && _jumpTimeoutDelta <= 0.0f)
+				{
+					// the square root of H * -2 * G = how much velocity needed to reach desired height
+					_verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+				}
 
 				// fall timeout
 				if (_fallTimeoutDelta >= 0.0f)
